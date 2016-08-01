f64913099c3a3657a0053047cf3dd25e0bb8d044
vit <vitaliydus@gmail.com>
2016-08-01 17:53:38 +0700

    fix tabs

--- src/components/league-fixtures.js
+++ src/components/league-fixtures.js
@@ -5,87 +5,87 @@ import { Link } from 'react-router';
 import { getTeamIdFromHref } from '../api';
 
 const renderRow = (item, homeTeam, awayTeam) => {
-	let result;
+  let result;
 
-	switch (item.status) {
-		case 'POSTPONED':
-			result = <small>Postponed</small>;
-			break;
-		case 'FINISHED':
-			result =
-				(item.result.goalsHomeTeam === null ? '-' : item.result.goalsHomeTeam)
-				+ ' : ' +
-				(item.result.goalsAwayTeam === null ? '-' : item.result.goalsAwayTeam)
-			break;
-		case 'TIMED':
-			const d = new Date(item.date);
-			const minutes = ('0' + d.getMinutes()).slice(-2);
-			const hours = ('0' + d.getHours()).slice(-2);
-			result = `${hours} : ${minutes}`;
-			break;
-		case 'IN_PLAY':
-			result = <div className="inplay">In Play</div>
-			break;
-		default:
-			result = '- : -';
-			break;
-	}
+  switch (item.status) {
+    case 'POSTPONED':
+      result = <small>Postponed</small>;
+      break;
+    case 'FINISHED':
+      result =
+        (item.result.goalsHomeTeam === null ? '-' : item.result.goalsHomeTeam)
+        + ' : ' +
+        (item.result.goalsAwayTeam === null ? '-' : item.result.goalsAwayTeam)
+      break;
+    case 'TIMED':
+      const d = new Date(item.date);
+      const minutes = ('0' + d.getMinutes()).slice(-2);
+      const hours = ('0' + d.getHours()).slice(-2);
+      result = `${hours} : ${minutes}`;
+      break;
+    case 'IN_PLAY':
+      result = <div className="inplay">In Play</div>
+      break;
+    default:
+      result = '- : -';
+      break;
+  }
 
-	return (
-		<div className="fixtures-row">
-			<div className="fixtures-home-team">
-				<Link to={`/team/${getTeamIdFromHref(item._links.homeTeam.href)}`}>
-					<div className="team-badge">
-						<div className="team-badge-name">
-							{item.homeTeamName}
-						</div>
-						<div className="team-logo-holder">
-							<img src={homeTeam && homeTeam.crestUrl} alt=""/>
-						</div>
-					</div>
-				</Link>
-			</div>
-			<div className="fixtures-result">
-				<div className="fixtures-result-title">
-					{result}
-				</div>
-			</div>
-			<div className="fixtures-away-team">
-				<Link to={`/team/${getTeamIdFromHref(item._links.awayTeam.href)}`}>
-					<div className="team-badge">
-						<div className="team-logo-holder">
-							<img src={awayTeam && awayTeam.crestUrl} alt=""/>
-						</div>
-						<div className="team-badge-name">
-							{item.awayTeamName}
-						</div>
-					</div>
-				</Link>
-			</div>
-		</div>
-	)
+  return (
+    <div className="fixtures-row">
+      <div className="fixtures-home-team">
+        <Link to={`/team/${getTeamIdFromHref(item._links.homeTeam.href)}`}>
+          <div className="team-badge">
+            <div className="team-badge-name">
+              {item.homeTeamName}
+            </div>
+            <div className="team-logo-holder">
+              <img src={homeTeam && homeTeam.crestUrl} alt=""/>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className="fixtures-result">
+        <div className="fixtures-result-title">
+          {result}
+        </div>
+      </div>
+      <div className="fixtures-away-team">
+        <Link to={`/team/${getTeamIdFromHref(item._links.awayTeam.href)}`}>
+          <div className="team-badge">
+            <div className="team-logo-holder">
+              <img src={awayTeam && awayTeam.crestUrl} alt=""/>
+            </div>
+            <div className="team-badge-name">
+              {item.awayTeamName}
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
 };
 
 const LeagueFixtures = (props) => {
-	let date = null;
+  let date = null;
 
-	const fixtures = props.fixtures.map((item, index) => {
-		const homeTeam = props.teams.find((team) => team.name === item.homeTeamName);
-		const awayTeam = props.teams.find((team) => team.name === item.awayTeamName);
+  const fixtures = props.fixtures.map((item, index) => {
+    const homeTeam = props.teams.find((team) => team.name === item.homeTeamName);
+    const awayTeam = props.teams.find((team) => team.name === item.awayTeamName);
 
-		const d = new Date(item.date.slice(0, 10));
-		const dateTitle = (date === d.getTime()) ? '' : <div className="fixtures-date">{dateformat(d, 'dddd d mmm yyyy')}</div>;
-		date = (date === d) ? date : d.getTime();
+    const d = new Date(item.date.slice(0, 10));
+    const dateTitle = (date === d.getTime()) ? '' : <div className="fixtures-date">{dateformat(d, 'dddd d mmm yyyy')}</div>;
+    date = (date === d) ? date : d.getTime();
 
-		return (
-			<div key={index + item.date}>
-				{dateTitle}
-				{renderRow(item, homeTeam, awayTeam)}
-			</div>
-		);
-	});
+    return (
+      <div key={index + item.date}>
+        {dateTitle}
+        {renderRow(item, homeTeam, awayTeam)}
+      </div>
+    );
+  });
 
-	return fixtures.length ? <div>{fixtures}</div> : <Loader />;
+  return fixtures.length ? <div>{fixtures}</div> : <Loader />;
 }
 
 export default LeagueFixtures;
--- src/containers/fixtures.js
+++ src/containers/fixtures.js
@@ -5,71 +5,71 @@ import LeagueFixtures from '../components/league-fixtures';
 
 class Results extends Component {
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			leagueId: null
-		}
-	}
+    this.state = {
+      leagueId: null
+    }
+  }
 
-	static contextTypes = {
-		router: React.PropTypes.object.isRequired
-	}
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
 
-	fetchData(leagueId) {
-		if (this.props.route.name === 'results') {
-			this.props.dispatch(fetchResults(leagueId));
-		} else {
-			this.props.dispatch(fetchFixtures(leagueId));
-		}
-	}
+  fetchData(leagueId) {
+    if (this.props.route.name === 'results') {
+      this.props.dispatch(fetchResults(leagueId));
+    } else {
+      this.props.dispatch(fetchFixtures(leagueId));
+    }
+  }
 
-	componentDidMount() {
-		const leagueId = this.props.league.id;
-		if (leagueId && leagueId != this.state.leagueId) {
-			this.setState({
-				leagueId
-			});
-			this.props.dispatch(fetchTeams(leagueId));
-			this.fetchData(leagueId);
-		}
-	}
+  componentDidMount() {
+    const leagueId = this.props.league.id;
+    if (leagueId && leagueId != this.state.leagueId) {
+      this.setState({
+        leagueId
+      });
+      this.props.dispatch(fetchTeams(leagueId));
+      this.fetchData(leagueId);
+    }
+  }
 
-	componentWillReceiveProps(nextProps) {
-		const leagueId = nextProps.league.id;
-		if (leagueId && leagueId != this.state.leagueId) {
-			this.setState({
-				leagueId
-			});
-			this.props.dispatch(fetchTeams(leagueId));
-			this.fetchData(leagueId);
-		}
-		if (leagueId && this.props.route.name !== nextProps.route.name) {
-			if (nextProps.route.name === 'results') {
-				this.props.dispatch(fetchResults(leagueId));
-			} else {
-				this.props.dispatch(fetchFixtures(leagueId));
-			}
-		} 
-	}
+  componentWillReceiveProps(nextProps) {
+    const leagueId = nextProps.league.id;
+    if (leagueId && leagueId != this.state.leagueId) {
+      this.setState({
+        leagueId
+      });
+      this.props.dispatch(fetchTeams(leagueId));
+      this.fetchData(leagueId);
+    }
+    if (leagueId && this.props.route.name !== nextProps.route.name) {
+      if (nextProps.route.name === 'results') {
+        this.props.dispatch(fetchResults(leagueId));
+      } else {
+        this.props.dispatch(fetchFixtures(leagueId));
+      }
+    }
+  }
 
-	render() {
-		const fixtures = this.props.route.name === 'results' ? this.props.results : this.props.fixtures;
-		return (
-			<LeagueFixtures fixtures={fixtures} teams={this.props.teams} />
-		)
-	}
+  render() {
+    const fixtures = this.props.route.name === 'results' ? this.props.results : this.props.fixtures;
+    return (
+      <LeagueFixtures fixtures={fixtures} teams={this.props.teams} />
+    )
+  }
 
 }
 
 const mapStateToProps = (state) => {
-	return {
-		league: state.league,
-		results: state.results,
-		teams: state.teams,
-		fixtures: state.fixtures
-	}
+  return {
+    league: state.league,
+    results: state.results,
+    teams: state.teams,
+    fixtures: state.fixtures
+  }
 }
 
 export default connect(mapStateToProps)(Results);
